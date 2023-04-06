903271a5e5c89b55456f61fe455f301b51bd8b9a
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2023-04-06 15:42:26 +0300

    Code de l'EJB

--- src/main/java/com/gnllsn/tpbanque52rasoanaivoginoallison/ejb/GestionnaireCompte.java
+++ src/main/java/com/gnllsn/tpbanque52rasoanaivoginoallison/ejb/GestionnaireCompte.java
@@ -8,23 +8,40 @@ package com.gnllsn.tpbanque52rasoanaivoginoallison.ejb;
  *
  * @author Grasoanaivo
  */
+import com.gnllsn.tpbanque52rasoanaivoginoallison.entities.CompteBancaire;
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
 
-@DataSourceDefinition (
-    className="com.mysql.cj.jdbc.MysqlDataSource",
-    name="java:app/jdbc/banque",
-    serverName="localhost",
-    portNumber=3306,
-    user="root",              // nom et
-    password="root", // mot de passe que vous avez donnés lors de la création de la base de données
-    databaseName="banque",
-    properties = {
-      "useSSL=false",
-      "allowPublicKeyRetrieval=true"
-    }
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et
+        password = "root", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true"
+        }
 )
 @Stateless
 public class GestionnaireCompte {
-    
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager entityManager;
+
+    public void creerCompte(CompteBancaire compte) {
+        entityManager.persist(compte);
+    }
+
+    public List<CompteBancaire> getAllCustomers() {
+       TypedQuery<CompteBancaire> query = entityManager.createNamedQuery("CompteBancaire.findAll",CompteBancaire.class);
+       return query.getResultList();
+    }
+
 }
